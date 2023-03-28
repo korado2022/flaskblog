825f4e61e488698e0d4c2ddcc618f03cecedee86
korado2022 <99610288+korado2022@users.noreply.github.com>
2023-03-28 21:27:58 +0300

    My homework for lesson 2 is ready (#2)

--- blog/app.py
+++ blog/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 
+from blog.article.views import articles_app
+from blog.user.views import users_app
 
-app = Flask(__name__)
-@app.route("/")
-def index():
-    return "Hello web!"
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(users_app)
+    app.register_blueprint(articles_app)
+
