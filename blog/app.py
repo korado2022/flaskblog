06b7c44466693bf26a4894e4cacc220412a1b0b1
korado2022 <99610288+korado2022@users.noreply.github.com>
2023-04-01 18:55:14 +0300

    My homework for lesson 3 is ready (#3)

--- blog/app.py
+++ blog/app.py
@@ -1,15 +1,33 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 from blog.article.views import articles_app
+from blog.auth.views import auth_app, login_manager
 from blog.user.views import users_app
+
+db = SQLAlchemy()
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = '-u%644akr4bplr*b6397=yj6^4-76#_#=2qpmwlpkh#-0zb1i_'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    # login_manager = LoginManager()
+    # login_manager.login_view = 'auth_app.login'
+    login_manager.init_app(app)
+
+    # from .models import User
+
     register_blueprints(app)
     return app
 
 def register_blueprints(app: Flask):
     app.register_blueprint(users_app)
     app.register_blueprint(articles_app)
+    app.register_blueprint(auth_app)
 
--- blog/user/views.py
+++ blog/user/views.py
@@ -1,25 +1,34 @@
 from flask import Blueprint, render_template
 from werkzeug.exceptions import NotFound
 
+
+
 users_app = Blueprint('users_app', __name__, url_prefix='/users', static_folder='../static')
 
 # USERS = ['Aleks', 'Jon', 'Ivan']
-USERS = {
-    1: 'Aleks',
-    2: 'Jon',
-    3: 'Ivan'
-}
+# USERS = {
+#     1: 'Aleks',
+#     2: 'Jon',
+#     3: 'Ivan'
+# }
 
 
 @users_app.route('/')
 def users_list():
-    return render_template('users/list.html', users=USERS)
+    from blog.models import User
+    users = User.query.all()
+    return render_template('users/list.html', users=users)
 
 @users_app.route('/<int:pk>')
-def get_user(pk: int):
-    try:
-        user_name = USERS[pk]
-    except KeyError:
-        raise NotFound(f'User id {pk} not found')
-    return render_template('users/details.html', user_name=user_name)
+def profile(pk: int):
+    # try:
+    #     user_name = USERS[pk]
+    # except KeyError:
+    #     raise NotFound(f'User id {pk} not found')
+    from blog.models import User
+    user = User.query.filter_by(id=pk).one_or_none()
+    if user is None:
+        raise NotFound(f"User #{pk} doesn't exist!")
+
+    return render_template('users/details.html', user=user)
 
